10fc5b446773b028187668fcf7c798677718e0fc
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2023-08-16 00:18:30 +0300

    Update register.validation.js

--- validation/register.validation.js
+++ validation/register.validation.js
@@ -4,7 +4,7 @@ const {  check } = require('express-validator')
 module.exports = [
     check('name').matches(/^[A-Za-z]+([\ A-Za-z-]+)*$/),
     check('email').isEmail(),
-    check('password').matches(),
+    check('password').matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/),
     check('PasswordConfirmation').custom((value, {req})=>{
         if(value!==req.body.password){
             return false
