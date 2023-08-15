a7582fd21224cfc9e139408d55bfd5e1ae9f9ac3
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2023-08-16 00:06:54 +0300

    Update register.routes.js

--- routes/register.routes.js
+++ routes/register.routes.js
@@ -1,5 +1,6 @@
 const app = require('express').Router()
 const { register, handleRegister } = require('../services/register.service');
+const validation = require('../validation/register.validation')
 
 app.get('/register', register)
 app.post('/handleRegister', handleRegister)
